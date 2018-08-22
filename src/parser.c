02ea5365229973da4d3301b15d324b13662b3918
Sebin Babu <sebin1998@gmail.com>
2018-08-22 14:36:45 +0530

    fixed major blunder

--- src/parser.c
+++ src/parser.c
@@ -23,19 +23,16 @@ int url_extract(char *str, int n, char **p) {
 
 		if(*c == '\0') {
 			int i = 0;
-
-			if(*c == '\0') {
+			while(*t != '\0' && is_whitespace(*t))
+				t++;
+			if(*t++ == '=') {
 				while(*t != '\0' && is_whitespace(*t))
 					t++;
-				if(*t++ == '=') {
-					while(*t != '\0' && is_whitespace(*t))
-						t++;
-					if(*t == '\'' || *t == '\"') {
-						quote = *t++;
-						while(*t != '\0' && *t != quote && i < n - 1)
-					 		str[i++] = *t++;
-						str[i] = '\0';
-					}
+				if(*t == '\'' || *t == '\"') {
+					quote = *t++;
+					while(*t != '\0' && *t != quote && i < n - 1)
+				 		str[i++] = *t++;
+					str[i] = '\0';
 				}
 			}
 			if(i != 0) break;
